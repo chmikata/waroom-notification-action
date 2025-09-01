0204ea593ad1241872b5f0ae88b74729554d4908
chmikata <chmikata@gmail.com>
2025-09-01 12:57:44 +0900

    判定ロジック修正

--- src/internal/application/logic.go
+++ src/internal/application/logic.go
@@ -51,10 +51,6 @@ func (i *IncidentNotificatin) Do(tpl string) error {
 }
 
 func (i *IncidentNotificatin) convert(tpl string, incidents []incident.Incident) (string, error) {
-	if len(incidents) == 0 {
-		return "{\"text\": \"*Incident Status*\n🎉There are No Incidents🎉\"}", nil
-	}
-
 	output, err := template.New("incident").Funcs(sprig.FuncMap()).Parse(tpl)
 	if err != nil {
 		return "", err
@@ -63,6 +59,10 @@ func (i *IncidentNotificatin) convert(tpl string, incidents []incident.Incident)
 	var targets []incident.Incident
 	for inc := range selectIncident(incidents) {
 		targets = append(targets, inc)
+	}
+
+	if len(targets) == 0 {
+		return "{\"text\": \"*Incident Status*\n🎉There are No Incidents🎉\"}", nil
 	}
 
 	buff := new(bytes.Buffer)
